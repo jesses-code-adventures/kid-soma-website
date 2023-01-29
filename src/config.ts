4aa3e8358d18c7e447250e92d8d73816f26efe34
Jesse Williams <jesse@negativespacesounds.com>
2023-01-29 20:29:33 +1100

    add navigation links, todo: fold to hamburger at smaller screen sizes

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const NavigationLinks = [
+    { name: "home", href: ''},
+    { name: "stems", href: 'stems'},
+    { name: "podcast", href: 'podcast'},
+    { name: "contact", href: 'contact'},
+]
+
+export const author = {
+    name: "jesse williams",
+    contact: "jesse@negativespacesounds.com",
+    description: "producer, piano & synth player, developer."
+}
